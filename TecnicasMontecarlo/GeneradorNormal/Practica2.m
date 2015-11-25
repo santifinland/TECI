e7df692d27cd7dd7f141601f190a007b3555e6f0
Santiago Dobón Méndez Trelles <sdmt@tid.es>
2015-11-25 10:55:46 +0100

    Add chi-test and exp and norm distribution generators

---
+++ TecnicasMontecarlo/GeneradorNormal/Practica2.m
@@ -0,0 +1,7 @@
+%Practica2
+
+% Generacion de distribución exponencial de parametro a =  1 / media
+distexp = GeneradorExponencial(100, 1 / 1.27)
+
+% Generacion de distribución normal de parametros m = media, v = varianza
+distnorm = GeneradorNormal(100, 6.03, 1.03)
